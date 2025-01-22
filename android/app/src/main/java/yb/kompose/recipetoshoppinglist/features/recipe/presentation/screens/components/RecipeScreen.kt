1a749e885e756e47f004755429ce51f8438f2190
Yoann Bourgault <bourgaulty@gmail.com>
2025-01-22 17:48:00 +0100

    Starting recipe detailed ui

--- android/app/src/main/java/yb/kompose/recipetoshoppinglist/features/recipe/presentation/screens/components/RecipeScreen.kt
+++ android/app/src/main/java/yb/kompose/recipetoshoppinglist/features/recipe/presentation/screens/components/RecipeScreen.kt
@@ -1,8 +1,15 @@
 package yb.kompose.recipetoshoppinglist.features.recipe.presentation.screens.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,9 +19,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import coil3.request.CachePolicy
+import coil3.request.ImageRequest
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import yb.kompose.recipetoshoppinglist.R
+import yb.kompose.recipetoshoppinglist.features.core.presentation.components.SectionTitle
 import yb.kompose.recipetoshoppinglist.features.recipe.domain.models.UiRecipe
 import yb.kompose.recipetoshoppinglist.features.recipe.presentation.recipes.vimos.RecipeViewModel
 
@@ -28,6 +46,8 @@ fun RecipeScreen(
 
     var recipe by remember { mutableStateOf<UiRecipe?>(null) }
 
+    val context = LocalContext.current
+
     LaunchedEffect(recipeId) {
         coroutineScope.launch(Dispatchers.Default) {
             recipeViewModel.getRecipeDetailed(recipeId).collect { r ->
@@ -39,18 +59,83 @@ fun RecipeScreen(
     Scaffold(
         modifier = modifier
     ) { innerPadding ->
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
             recipe?.let { r ->
+                item {
+                    AsyncImage(
+                        model = ImageRequest.Builder(context)
+                            .data(r.imgUrl)
+                            .memoryCachePolicy(CachePolicy.ENABLED)
+                            .build(),
+                        contentDescription = r.title,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .aspectRatio(16f / 9f)
+                    )
+                }
+                item {
+                    SectionTitle(
+                        title = r.title,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 32.dp),
+                        textAlign = TextAlign.Center
+                    )
+                }
+                r.instructions?.let { instructions ->
+                    item {
+                        Text(
+                            text = instructions,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp)
+                                .padding(bottom = 32.dp)
+                        )
+                    }
+                }
+                if (r.ingredients.isNotEmpty()) {
+                    item {
+                        SectionTitle(
+                            title = stringResource(R.string.ingredients),
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center
+                        )
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                        ) {
+                            r.ingredients.forEach { ingredient ->
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ) {
+                                    Text(
+                                        text = ingredient.name
+                                    )
+                                    Spacer(
+                                        modifier = Modifier.width(16.dp)
+                                    )
+                                    Text(
+                                        text = ingredient.amount
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            } ?: item {
                 Text(
-                    text = "Name = ${r.title}"
+                    text = "No Recipe found with id $recipeId"
                 )
-            } ?: Text(
-                text = "No Recipe"
-            )
+            }
         }
     }
 
