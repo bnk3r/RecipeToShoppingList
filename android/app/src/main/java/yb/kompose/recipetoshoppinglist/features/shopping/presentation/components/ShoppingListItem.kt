407fc153a27233219b21d8da41b1d235a13ec0b0
Yo <bourgaulty@gmail.com>
2025-03-04 16:39:16 +0100

    Shopping lists screen rework

--- android/app/src/main/java/yb/kompose/recipetoshoppinglist/features/shopping/presentation/components/ShoppingListItem.kt
+++ android/app/src/main/java/yb/kompose/recipetoshoppinglist/features/shopping/presentation/components/ShoppingListItem.kt
@@ -1,38 +1,68 @@
 package yb.kompose.recipetoshoppinglist.features.shopping.presentation.components
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import yb.kompose.recipetoshoppinglist.R
+import yb.kompose.recipetoshoppinglist.features.core.presentation.components.text.SectionTitle
 import yb.kompose.recipetoshoppinglist.features.shopping.domain.models.UiShoppingList
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun ShoppingListItem(
-    modifier: Modifier = Modifier,
     shoppingList: UiShoppingList,
-    onClick: () -> Unit
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
     Card(
         modifier = modifier,
         onClick = onClick,
         border = when (shoppingList.current) {
-            true -> BorderStroke(4.dp, Color.Red)
+            true -> BorderStroke(2.dp, MaterialTheme.colorScheme.primary)
             false -> null
-        }
+        },
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.onSurface
+        )
     ) {
         Box(
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            Text(
-                text = shoppingList.id.toString()
-            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                SectionTitle(
+                    title = stringResource(R.string.update)
+                )
+                SectionTitle(
+                    title = shoppingList.updatedDate.format(
+                        DateTimeFormatter.ofPattern("dd/MM/yyyy")
+                    )
+                )
+                Text(
+                    text = stringResource(
+                        R.string.ingredients_count,
+                        shoppingList.ingredients.size
+                    )
+                )
+            }
         }
     }
 }
--- android/app/src/main/java/yb/kompose/recipetoshoppinglist/features/shopping/presentation/components/ShoppingListsScreen.kt
+++ android/app/src/main/java/yb/kompose/recipetoshoppinglist/features/shopping/presentation/components/ShoppingListsScreen.kt
@@ -1,5 +1,6 @@
 package yb.kompose.recipetoshoppinglist.features.shopping.presentation.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
@@ -7,14 +8,22 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import yb.kompose.recipetoshoppinglist.R
+import yb.kompose.recipetoshoppinglist.features.core.presentation.components.button.DesignButton
 import yb.kompose.recipetoshoppinglist.features.shopping.presentation.models.states.ShoppingListsScreenState
 
 @Composable
@@ -34,8 +43,10 @@ fun ShoppingListsScreen(
         item(
             span = { GridItemSpan(2) }
         ) {
-            AddShoppingListButton(
+            DesignButton(
                 onClick = onClickAddNewList,
+                text = stringResource(R.string.new_list).uppercase(),
+                imageVector = Icons.Default.Add,
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(56.dp)
@@ -58,6 +69,7 @@ fun ShoppingListsScreen(
             )
         }
     }
+
 }
 
 @Preview(showSystemUi = true, backgroundColor = 0xffffffff)
